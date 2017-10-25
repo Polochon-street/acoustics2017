e85104f8d3b1498902beef34698f5eb2700697ce
Polochon-street <polochonstreet@gmx.fr>
2017-10-25 21:46:30 +0200

    Added tau detection + plots

--- Lecture_06_Module_01.m
+++ Lecture_06_Module_01.m
@@ -53,18 +53,86 @@ hR = h(:, 2);
 xL = convolveFFT_OLS(s, hL);
 xR = convolveFFT_OLS(s, hR);
 
-tauSec = 0.1;
+tauSec = 1;
 winSec = 8e-3;
 fs = fsHz;
+
+% First computations to do DDR stuff
+brir = cat(2, hR, hL);
+hDirect = splitBRIR(brir, fs);
+hDirectL = hDirect(:, 1);
+hDirectR = hDirect(:,2);
+sLDry = convolveFFT_OLS(s, hDirectL);
+sRDry = convolveFFT_OLS(s, hDirectR);
 
 %% PERFORM COHERENCE-BASED DEREVERBERATION
 [sL, sR, G] = dereverb(xL, xR, fs, winSec, tauSec);
 
-% Read sL and sR for the output
+%% Finding the best "tau"
+DeltaDDRs = [];
+tauSecs = [];
+for tauSec = 10e-3:0.01:1
+    [sL, sR, G] = dereverb(xL, xR, fs, winSec, tauSec);
+    DDR_pre = 10*log10((sum(sLDry.*sLDry) + sum(sRDry.*sRDry)) / ...
+        (sum((xL-sLDry).*(xL-sLDry) + (xR - sRDry).*(xR - sRDry))));
+
+    DDR_post = 10*log10((sum(sLDry.*sLDry) + sum(sRDry.*sRDry)) / ...
+        (sum((sL - sLDry).*(sL - sLDry) + (xR -sRDry).*(xR - sRDry))));
+    DeltaDDR = DDR_post - DDR_pre;
+    tauSecs = [tauSec, tauSecs];
+    DeltaDDRs = [DeltaDDR, DeltaDDRs];
+end
+
+[DeltaDDR, index] = max(DeltaDDRs);
+tauSec = tauSecs(index);
+fprintf('Highest DeltaDDR is %f for tauSec = %f\n', DeltaDDR, tauSec);
 
 %% PERFORMING EVALUATION
-brir = cat(2, hR, hL);
-hDirect = splitBRIR(brir, fs);
+
+N = floor(winSec * fs); % could be floor or ceil
+R = floor(N * 0.25); % could be floor or ceil
+M = pow2(nextpow2(N));
+w = cola(N, R, 'hamming', 'wola');
+
+[S,t,f] = stft(s, fs, w, R, M);
+[XR] = stft(xR, fs, w, R, M);
+[SR] = stft(sR, fs, w, R, M);
+
+% positive frequencies
+f = f((size(f)/2)+1:end);
+S = S(size(f):end,:);
+XR = XR(size(f):end,:);
+SR = SR(size(f):end,:);
+
+% plots
+figure;
+imagesc(t,f,10*log10((abs(S).^2)/(abs(max(max(S)))^2)));
+caxis([-60,0]);
+colorbar;
+title('Dry');
+xlabel('time, s'); ylabel('frequency, Hz');
+figure;
+imagesc(t,f,10*log10((abs(XR).^2)/(abs(max(max(XR)))^2)));
+caxis([-60,0]);
+colorbar;
+title('Reverb');
+xlabel('time, s'); ylabel('frequency, Hz');
+figure;
+imagesc(t,f,10*log10((abs(SR).^2)/(abs(max(max(SR)))^2)));
+caxis([-60,0]);
+colorbar;
+title('Dereverb');
+xlabel('time, s'); ylabel('frequency, Hz');
+
+%% SIGNAL PLOT
+
+figure;
+plot(xR,'r');
+hold;
+plot(sR,'g');
+title('Signals');
+legend('Reverb','Enhanced');
+f = f((size(f)/2)+1:end);
 
 function C = estCohere(XL, XR, alpha)
     phiLL = XL .* conj(XL);
